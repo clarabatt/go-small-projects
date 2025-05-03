8849080dac10c45c7994833a04dee3dc3da6939d
Clara Battesini <clarabattesini@gmail.com>
2025-05-03 10:54:25 -0400

    creating a menu to run the examples

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"project/internal/ch1"
 )
 
+var optionsChapter1 = map[string]func(){
+	"echo1":     ch1.Echo1,
+	"echo2":     ch1.Echo2,
+	"echo3":     ch1.Echo3,
+	"dup1":      ch1.Dup1,
+	"dup3":      ch1.Dup3,
+	"map1":      ch1.Map1,
+	"lissajous": ch1.Lissajous,
+}
+
 func main() {
-	// ch1.Echo1()
-	// ch1.Echo2()
-	// ch1.Echo3()
+	fmt.Println("=== Welcome! ===")
+	printMenu()
+}
 
-	// ch1.Dup1()
-	// ch1.Dup3()
+func printMenu() {
+	var choice int
 
-	// ch1.Map1()
+	for {
+		fmt.Println("Chapter 1: ")
+		mappedOptions := make(map[int]string)
+		optionCounter := 1
 
-	ch1.Lissajous()
+		for key := range optionsChapter1 {
+			fmt.Printf("    %d. %s\n", optionCounter, key)
+			mappedOptions[optionCounter] = key
+			optionCounter++
+		}
+		fmt.Print("> ")
+
+		_, err := fmt.Scanln(&choice)
+		if err != nil {
+			fmt.Scanln() // clear the invalid input
+			fmt.Println("Invalid input, please enter a number:")
+			continue
+		}
+
+		if choice <= 0 || choice > optionCounter-1 {
+			fmt.Println("Out of range, choose again:")
+		} else {
+			optionsChapter1[mappedOptions[choice]]()
+			break
+		}
+	}
 }
